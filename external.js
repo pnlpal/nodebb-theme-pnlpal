2410163f0a5f9d89e23b6051e81cec65decaee3f
River <revir.qing@gmail.com>
2024-11-05 11:45:30 +0100

    try og twitter:card

--- external.js
+++ external.js
@@ -109,6 +109,11 @@ module.exports = function (library) {
 				noEscape: true,
 			});
 			res.locals.metaTags.push({
+				property: 'twitter:card',
+				content: 'https://i.ytimg.com/vi/' + parsed.videoId + '/hqdefault.jpg',
+				noEscape: true,
+			});
+			res.locals.metaTags.push({
 				property: 'og:image',
 				content:
 					'https://i.ytimg.com/vi/' + templateData.videoId + '/hqdefault.jpg',
