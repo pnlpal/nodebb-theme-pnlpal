993c0b7e90748d663ede8da015d2e92e3f2f23f3
River <revir.qing@gmail.com>
2025-07-15 11:33:41 +0200

    fix the deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 # Variables
-SERVER="your.server.com"
-USER="your_ssh_user"
+SERVER="167.172.46.156"
+PORT="4500"
+USER="river"
 PROJECT_DIR="/home/river/pnlpal/NodeBB"
 
 # SSH and run commands
-ssh ${USER}@${SERVER} << EOF
+ssh ${USER}@${SERVER} -p${PORT} << EOF
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
   cd ${PROJECT_DIR}
   yarn upgrade nodebb-theme-pnlpal
   ./nodebb build
